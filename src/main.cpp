cc943f2a82dc632e9113e0d309e17c73da8f0e09
Sam Haves <haves.sam@gmail.com>
2016-03-09 01:20:11 -0500

    ported some old code, created Makefile, linked SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+//
+//  main.cpp
+//  Asuna
+//  Created By Sam Haves
+//
+
 #include <iostream>
 
-int main(){
 
+int main(int argc, const char * argv[]) {
+  SDL_init(SDL_INIT_EVERYTHING);
+  SDL_Quit();
   return 0;
 }
+
