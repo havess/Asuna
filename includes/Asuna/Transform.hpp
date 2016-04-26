a49e2ed7123f641cb0d906f248b6320f469353d3
Sam Haves <haves.sam@gmail.com>
2016-04-25 23:25:22 -0400

    and the refactoring begins

--- includes/Asuna/Transform.hpp
+++ includes/Asuna/Transform.hpp
@@ -13,37 +13,37 @@
 #include "glm/glm.hpp"
 #include "glm/gtx/transform.hpp"
 
+typedef glm::mat4 mat4;
+typedef glm::vec3 vec3;
+
 class Transform{
 public:
-    
-    Transform(const glm::vec3& pos = glm::vec3(), const glm::vec3& rot  = glm::vec3(), const glm::vec3& scale  = glm::vec3(1.0f, 1.0f, 1.0f)):
-    m_pos(pos),
-    m_rot(rot),
-    m_scale(scale) {}
-    
-    inline glm::mat4 getModel() const{
-        glm::mat4 posMatrix = glm::translate(m_pos);
-        glm::mat4 rotXMatrix = glm::rotate(m_rot.x, glm::vec3(1,0,0));
-        glm::mat4 rotYMatrix = glm::rotate(m_rot.y, glm::vec3(0,1,0));
-        glm::mat4 rotZMatrix = glm::rotate(m_rot.z, glm::vec3(0,0,1));
-        glm::mat4 scaleMatrix = glm::scale(m_scale);
-        
-        glm::mat4 rotMatrix = rotZMatrix * rotYMatrix * rotXMatrix;
-        
+
+    Transform(const vec3& pos = vec3(), const vec3& rot  = vec3(), const vec3& scale  = vec3(1.0f, 1.0f, 1.0f)): m_pos(pos),m_rot(rot),m_scale(scale) {}
+
+    inline mat4 getModel() const{
+        mat4 posMatrix = glm::translate(m_pos);
+        mat4 rotXMatrix = glm::rotate(m_rot.x, vec3(1,0,0));
+        mat4 rotYMatrix = glm::rotate(m_rot.y, vec3(0,1,0));
+        mat4 rotZMatrix = glm::rotate(m_rot.z, vec3(0,0,1));
+        mat4 scaleMatrix = glm::scale(m_scale);
+
+        mat4 rotMatrix = rotZMatrix * rotYMatrix * rotXMatrix;
+
         return posMatrix * rotMatrix * scaleMatrix;
     }
-    
-    inline glm::vec3& getPos(){return m_pos;}
-    inline glm::vec3& getRot(){return m_rot;}
-    inline glm::vec3& getScale(){return m_scale;}
-    
-    inline void setPos(const glm::vec3& pos){m_pos = pos;}
-    inline void setRot(const glm::vec3& rot){m_rot = rot;}
-    inline void setScale(const glm::vec3& scale){m_scale = scale;}
+
+    inline vec3& getPos(){return m_pos;}
+    inline vec3& getRot(){return m_rot;}
+    inline vec3& getScale(){return m_scale;}
+
+    inline void setPos(const vec3& pos){m_pos = pos;}
+    inline void setRot(const vec3& rot){m_rot = rot;}
+    inline void setScale(const vec3& scale){m_scale = scale;}
 protected:
 private:
-    glm::vec3 m_pos;
-    glm::vec3 m_rot;
-    glm::vec3 m_scale;
+    vec3 m_pos;
+    vec3 m_rot;
+    vec3 m_scale;
 };
 #endif /* Transform_hpp */
